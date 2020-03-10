ce7051dd9b4978cd686f6e468b74245505f62ade
Michael Du <mdu7@stanford.edu>
2020-03-10 16:18:31 -0700

    torch.cud.long -> torch.tensor

--- linguistic_style_transfer_pytorch/model.py
+++ linguistic_style_transfer_pytorch/model.py
@@ -255,7 +255,7 @@ class AdversarialVAE(nn.Module):
             style_emb: batch of sampled style embeddings of the input sentences,shape = (batch_size,mconfig.style_hidden_dim)
             style_labels: style labels of the corresponding input sentences,shape = (batch_size,2)
         """
-        neg_style_label = torch.cuda.LongTensor([0, 1], device=style_emb.device)
+        neg_style_label = torch.tensor([0, 1], device=style_emb.device, dtype=torch.long)
         # Iterate over the style labels
         for idx, label in enumerate(style_labels):
             # Calculate average for negative style
@@ -423,8 +423,8 @@ class AdversarialVAE(nn.Module):
         # Training mode
         if not inference:
             # Prepend the input sentences with <sos> token
-            sos_token_tensor = torch.cuda.LongTensor(
-                [gconfig.predefined_word_index['<sos>']], device=input_sentences.device).unsqueeze(0).repeat(mconfig.batch_size, 1)
+            sos_token_tensor = torch.tensor(
+                [gconfig.predefined_word_index['<sos>']], device=input_sentences.device, dtype=torch.long).unsqueeze(0).repeat(mconfig.batch_size, 1)
             input_sentences = torch.cat(
                 (sos_token_tensor, input_sentences), dim=1)
             sentence_embs = self.dropout(self.embedding(input_sentences))
@@ -452,8 +452,8 @@ class AdversarialVAE(nn.Module):
         # if inference mode is on
         else:
 
-            sos_token_tensor = torch.cuda.LongTensor(
-                [gconfig.predefined_word_index['<sos>']], device=latent_emb.device).unsqueeze(0)
+            sos_token_tensor = torch.tensor(
+                [gconfig.predefined_word_index['<sos>']], device=latent_emb.device, dtype=torch.long).unsqueeze(0)
             word_emb = self.embedding(sos_token_tensor)
             hidden_states = torch.zeros(
                 1, mconfig.hidden_dim, device=latent_emb.device)
